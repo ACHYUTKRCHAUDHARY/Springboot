16374d9c67e8bd42eb8eb8490fa5c3a91ee926ed
achyut <achyutchaudhary47@gmail.com>
2026-04-04 18:10:46 +0530

    allargsconstructor

--- src/main/java/org/example/spring2/controller/StudentController.java
+++ src/main/java/org/example/spring2/controller/StudentController.java
@@ -1,10 +1,13 @@
 package org.example.spring2.controller;
 
+import org.example.spring2.dto.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     @GetMapping("/student")
-    public
+    public Student getStudent() {
+        return new Student(4,"Achyut",achyutchaudhary@gmail.com);
+    }
 }
--- src/main/java/org/example/spring2/dto/Student.java
+++ src/main/java/org/example/spring2/dto/Student.java
@@ -1,12 +1,15 @@
 package org.example.spring2.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * data annotation is used to for the bringing/creating the data from the getter
  * setter and constructor and hashcode
+ * AllArgsConstructor is used to create the constructor with all the arguments
  */
 @Data
+@AllArgsConstructor
 public class Student {
     private Long id;
     private String name;
